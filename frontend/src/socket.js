1d6340b25844501eee03cb5984b786dcc2fb2563
Sahil-repositery <khans549897@gmail.com>
2026-06-17 18:21:08 +0530

    socket url

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 const SOCKET_URL = import.meta.env.VITE_WS_URL || (
   window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
     ? "http://localhost:5000"
-    : window.location.origin
+    : "https://scribble-io-game.onrender.com"
 );
 
 export const socket = io(SOCKET_URL, {
